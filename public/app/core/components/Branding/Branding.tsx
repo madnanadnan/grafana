a3c58f3ea8fd3f41419c30d6c8d1390e211e57cb
Mustafa Adnan <muss@Mustafas-MacBook-Pro.local>
2020-10-09 14:35:52 +0100

    first commit

--- public/app/core/components/Branding/Branding.tsx
+++ public/app/core/components/Branding/Branding.tsx
@@ -8,21 +8,27 @@ export interface BrandComponentProps {
 }
 
 const LoginLogo: FC<BrandComponentProps> = ({ className }) => {
-  return <img className={className} src="public/img/grafana_icon.svg" alt="Grafana" />;
+  return <img className={className} src="public/img/ics_mini_logo" alt="Grafana" />;
 };
 
 const LoginBackground: FC<BrandComponentProps> = ({ className, children }) => {
-  const theme = useTheme();
+  //const theme = useTheme();
   const background = css`
-    background: url(public/img/login_background_${theme.isDark ? 'dark' : 'light'}.svg);
-    background-size: cover;
+    background: rgb(0, 212, 255);
+    background: radial-gradient(
+      circle,
+      rgba(0, 212, 255, 1) 3%,
+      rgba(9, 9, 121, 1) 88%,
+      rgba(115, 179, 255, 1) 91%,
+      rgba(2, 0, 36, 1) 100%
+    );
   `;
 
   return <div className={cx(background, className)}>{children}</div>;
 };
 
 const MenuLogo: FC<BrandComponentProps> = ({ className }) => {
-  return <img className={className} src="public/img/grafana_icon.svg" alt="Grafana" />;
+  return <img className={className} src="public/img/ics_mini_logo" alt="Grafana" />;
 };
 
 const LoginBoxBackground = () => {
@@ -38,15 +44,10 @@ export class Branding {
   static LoginBackground = LoginBackground;
   static MenuLogo = MenuLogo;
   static LoginBoxBackground = LoginBoxBackground;
-  static AppTitle = 'Grafana';
-  static LoginTitle = 'Welcome to Grafana';
+  static AppTitle = 'Metrics Manager';
+  static LoginTitle = 'Welcome to Metrics Manager';
   static GetLoginSubTitle = () => {
-    const slogans = [
-      "Don't get in the way of the data",
-      'Your single pane of glass',
-      'Built better together',
-      'Democratising data',
-    ];
+    const slogans = ['The Intrepid Way', 'Your single pane of glass', 'Built better together', 'Democratising data'];
     const count = slogans.length;
     return slogans[Math.floor(Math.random() * count)];
   };
